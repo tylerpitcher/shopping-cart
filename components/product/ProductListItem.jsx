82afc83510e0328b430301319378bc9be3d36176
Tyler Pitcher <20trlp@queensu.ca>
2023-03-20 19:24:39 -0400

    Include short description in item

--- components/product/ProductListItem.jsx
+++ components/product/ProductListItem.jsx
@@ -8,7 +8,7 @@ function ProductListItem({ product }) {
       <div className={styles.infoWrapper}>
         <div className={styles.description}>
           <p>
-            Freestart lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id nibh sem. Vivamus pretium erat ac magna sodales, vitae vestibulum lectus vehicula. Proin a libero non velit pharetra venenatis. Ut tristique consectetur turpis ut tincidunt. In feugiat at lectus a accumsan. Sed purus nulla, vehicula vel pulvinar ac, suscipit vel dui. Etiam aliquet ante ut nunc lobortis suscipit sed sit amet nisi. Sed in nisi quam. Aliquam faucibus pretium justo at porttitor. Nam sit amet dapibus mi. Proin elementum ullamcorper ante, id commodo sem lobortis a. Donec sit amet rutrum tortor, quis tristique augue. In hendrerit quam nec lacinia molestie. Praesent et ultrices nisi. Sed nec elementum magna.
+            {product.shortDescription}
           </p>
           <div className={styles.priceWrapper}>
             <span className={styles.price}>{product.price.toPriceString()}</span>
